80fd4c686ef211839ce3b67f2940cbedaa14458e
PhobiaCide <aamason86@gmail.com>
2025-03-23 15:39:14 -0500

    update

--- src/eve-image-helper.js
+++ src/eve-image-helper.js
@@ -1,50 +1,72 @@
 class Image {
   constructor(id) {
     this.id = id;
-    this.url = null;
     this.variants = {};
   }
 
   static async create(id) {
     const instance = new Image(id);
-    const res = await fetch(`https://esi.evetech.net/latest/universe/names/`, {
+    const base = 'https://images.evetech.net';
+    const esi = 'https://esi.evetech.net/latest';
+
+    const nameRes = await fetch(`${esi}/universe/names/`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify([id])
     });
 
-    const [{ category }] = await res.json();
-    const base = 'https://images.evetech.net';
+    const [nameData] = await nameRes.json();
 
-    const allVariants = {
-      alliance: [`${base}/alliances/${id}/logo`],
-      corporation: [`${base}/corporations/${id}/logo`],
-      faction: [`${base}/corporations/${id}/logo`],
-      character: [`${base}/characters/${id}/portrait`],
-      inventory_type: [
-        `${base}/types/${id}/render`,
-        `${base}/types/${id}/icon`,
-        `${base}/types/${id}/bp`,
-        `${base}/types/${id}/bpc`,
-        `${base}/types/${id}/relic`
-      ]
-    }[category] || [];
+    if (nameData?.category === 'inventory_type') {
+      const typeRes = await fetch(`${esi}/universe/types/${id}/`);
+      const typeData = await typeRes.json();
+      const groupId = typeData.group_id;
 
-    const validEntries = await Promise.all(
-      allVariants.map(async url => [url, await Image.checkImageExists(url)])
-    );
+      const groupRes = await fetch(`${esi}/universe/groups/${groupId}/`);
+      const groupData = await groupRes.json();
+      const categoryId = groupData.category_id;
 
-    instance.variants = Object.fromEntries(
-      validEntries.filter(([, ok]) => ok)
-    );
+      const categoryRes = await fetch(`${esi}/universe/categories/${categoryId}/`);
+      const categoryData = await categoryRes.json();
 
-    instance.url = Object.keys(instance.variants)[0] || null;
+      // Use category to determine valid image types
+      switch (categoryId) {
+        case 6: // Ship
+          instance.variants.render = `${base}/types/${id}/render`;
+          break;
+        case 16: // Ancient Relics
+          instance.variants.relic = `${base}/types/${id}/relic`;
+          break;
+        case 9: // Blueprint
+          if (groupId === 332) {
+            instance.variants.bp = `${base}/types/${id}/bp`;
+          } else if (groupId === 333) {
+            instance.variants.bpc = `${base}/types/${id}/bpc`;
+          }
+          break;
+        default:
+          instance.variants.icon = `${base}/types/${id}/icon`;
+          break;
+      }
+
+    } else {
+      const category = nameData?.category;
+      switch (category) {
+        case 'alliance':
+          instance.variants.logo = `${base}/alliances/${id}/logo`;
+          break;
+        case 'corporation':
+        case 'faction':
+          instance.variants.logo = `${base}/corporations/${id}/logo`;
+          break;
+        case 'character':
+          instance.variants.portrait = `${base}/characters/${id}/portrait`;
+          break;
+      }
+    }
 
     return instance;
   }
-
-  static checkImageExists = url =>
-    fetch(url, { method: 'HEAD' })
-      .then(res => res.ok)
-      .catch(() => false);
 }
+
+window.Image = Image;
